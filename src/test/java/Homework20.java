1bd3cb5a5d334d5b659174ba7caf3226f446876b
Rumenul Rimon <77926360+shield75@users.noreply.github.com>
2024-11-05 22:03:31 +0600

    Homework #20: Selenium Waits

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -1,3 +1,11 @@
-public class Homework20 {
+import org.testng.annotations.Test;
 
+public class Homework20 extends BaseTest {
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        provideEmail("rumenul.rimon@testpro.io");
+        providePassword("27041575");
+        clickSubmit();
+        deleteAPlaylist();
+    }
 }
